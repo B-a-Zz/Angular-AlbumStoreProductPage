73ecc344d0e49c146083bbadc23f4efac713d008
B-a-Zz <clement.f.bazz@gmail.com>
2020-09-26 07:42:51 +0100

    updating

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,10 +3,15 @@
 import { Injectable } from '@angular/core';
 import { from } from 'rxjs/observable/from';
 import{Http, Response} from '@angular/http'
+import{} from 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
-private _albumUrl="../assets/album.json"
+  private _albumUrl= "../assets/album.json"
+
   constructor(private _http: Http) { }
+
+
+  getAlbum(id:number) { return this._http.get(this._albumUrl)}
 
 }
